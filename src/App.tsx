11112d6347c8c771d4c7a7a44f761270cde372ca
sunbeamhub <sunbeamhub@outlook.com>
2022-01-25 19:31:36 +0800

    chore: integrate microsoft fast

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
+import {
+  provideFASTDesignSystem,
+  fastCard,
+  fastButton,
+} from '@microsoft/fast-components';
+import { provideReactWrapper } from '@microsoft/fast-react-wrapper';
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+const { wrap } = provideReactWrapper(React, provideFASTDesignSystem());
+
+export const FastCard = wrap(fastCard());
+export const FastButton = wrap(fastButton());
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <FastCard>
+      <h2>FAST React</h2>
+      <FastButton appearance="accent" onClick={() => console.log('clicked')}>
+        Click Me
+      </FastButton>
+    </FastCard>
   );
 }
 
