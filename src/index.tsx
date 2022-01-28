0568a6e56c4c72c9a10d2bc0ba8a1d9ffa3217e9
sunbeamhub <sunbeamhub@outlook.com>
2022-01-28 16:31:28 +0800

    chore: add development and deployment config

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,36 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { ReportHandler } from 'web-vitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function sendToAnalytics(metric?: ReportHandler) {
+  const body = JSON.stringify(metric);
+  const url = 'https://example.com/analytics';
+
+  // Use `navigator.sendBeacon()` if available, falling back to `fetch()`
+  if (navigator.sendBeacon) {
+    navigator.sendBeacon(url, body);
+  } else {
+    fetch(url, { body, method: 'POST', keepalive: true });
+  }
+}
+
+reportWebVitals(
+  process.env.NODE_ENV === 'production'
+    ? sendToAnalytics
+    : process.env.NODE_ENV === 'development'
+    ? console.log
+    : undefined
+);
